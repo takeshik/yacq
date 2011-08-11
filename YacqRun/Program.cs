b898e3622481640e7422aa1fa4f20a104df23468
Takeshi KIRIYA <takeshik@xspect.org>
2011-08-11 17:33:53 +0900

    Rewrite YacqRun

--- YacqRun/Program.cs
+++ YacqRun/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Linq.Expressions;
@@ -9,75 +10,136 @@ namespace XSpect.Yacq.Runner
 {
     internal class Program
     {
-        private static void Main(String[] args)
+        private static Int32 Main(String[] args)
         {
             if (args.Length == 0)
             {
                 Repl();
+                return 0;
             }
             else if (args[0] == "-")
             {
-                ReadAsScript(Console.In);
+                return ReadAsScript(Console.In);
             }
             else
             {
-                ReadAsScript(new StreamReader(args[0]));
+                return ReadAsScript(new StreamReader(args[0]));
             }
         }
 
-        private static void ReadAsScript(TextReader input)
+        private static Int32 ReadAsScript(TextReader input)
         {
             var head = input.ReadLine();
-            Expression.Lambda<Action>(
-                Yacq.Parse((head.StartsWith("#!") ? "" : head) + input.ReadToEnd())
-            ).Compile()();
+            var ret =  Run((head.StartsWith("#!") ? "" : head) + input.ReadToEnd(), false);
+            return ret is Int32 ? (Int32) ret : 0;
         }
 
         private static void Repl()
         {
-            String code = "";
+            String heredoc = null;
+            var code = "";
+            Console.WriteLine(
+                #region String
+@"Yacq Runner (REPL Mode)
+Type \help [ENTER] to show help."
+                #endregion
+            );
             while (true)
             {
-                Console.Write("yacq> ");
+                if (heredoc == null)
+                {
+                    Console.Write("yacq> ");
+                }
+                Console.ForegroundColor = ConsoleColor.White;
                 var input = Console.ReadLine();
-                if (input.StartsWith("\\"))
+                Console.ResetColor();
+                if (heredoc != null)
+                {
+                    if (input == heredoc)
+                    {
+                        Run(code, true);
+                        heredoc = null;
+                    }
+                    else
+                    {
+                        code += input + Environment.NewLine;
+                    }
+                }
+                else if (input.StartsWith("\\"))
                 {
                     switch (input.Substring(1))
                     {
                         case "exit":
                             Environment.Exit(0);
                             break;
-                        case "run":
-                            try
-                            {
-                                var expr = Yacq.Parse(code);
-                                Console.WriteLine("Expression: ");
-                                Console.WriteLine("  " + expr);
-                                Expression.Lambda<Action>(expr).Compile()();
-                            }
-                            catch (Exception ex)
-                            {
-                                Console.WriteLine(ex);
-                            }
-                            finally
-                            {
-                                code = "";
-                            }
+                        case "help":
+                            Console.WriteLine(
+                            #region String
+@"Commands:
+  \exit
+    Exit this program.
+  \help
+    Show this message.
+  \debug
+    Attach the debugger.
+  \gc
+    Run GC manually.
+  (CODE)
+    Run one-line CODE.
+  <<(INPUT) [ENTER] (CODES)
+    Run multi-line CODES while INPUT line was got (heredoc <<EOT).
+  (CODE)
+    Otherwise: Run one-line code."
+                            #endregion
+);
                             break;
-                        case "clear":
-                            code = "";
+                        case "debug":
+                            Debugger.Launch();
                             break;
-                        case "show":
-                            Console.WriteLine(code);
-                            break;
-                        default:
+                        case "gc":
+                            GC.Collect();
                             break;
                     }
                 }
+                else if (input.StartsWith("<<"))
+                {
+                    heredoc = input.Substring(2);
+                }
                 else
                 {
-                    code += Environment.NewLine + input;
+                    Run(input, true);
                 }
+            }
+        }
+
+        private static Object Run(String code, Boolean showInfo)
+        {
+            try
+            {
+                var expr = Yacq.Parse(code);
+                if (showInfo)
+                {
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                    Console.Write("Expression: ");
+                    Console.WriteLine(expr);
+                    Console.ResetColor();
+                }
+                var ret = Expression.Lambda(expr).Compile().DynamicInvoke();
+                if (showInfo)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.Write("Returns: ");
+                    Console.WriteLine(ret ?? "(null)");
+                    Console.ResetColor();
+                }
+                return ret;
+            }
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.WriteLine(ex);
+                Console.ResetColor();
+                return null;
             }
         }
     }
