7d838bd6b00ad032734c3f3125e9523ded1353cc
Takeshi KIRIYA <takeshik@xspect.org>
2011-11-20 01:08:06 +0900

    Fix implementations in ReplSymbols
    Fix implementations of exported Yacq symbols in ReplSymbols class, Rewrite
    output codes to expression generating codes, not to run them only in expression
    evaluating time.

--- YacqRun/ReplSymbols.cs
+++ YacqRun/ReplSymbols.cs
@@ -44,6 +44,94 @@ namespace XSpect.Yacq.Runner
 {
     internal static class ReplSymbols
     {
+        private const String _help =
+            #region String
+@"Commands:
+  (!exit)
+    Exit this program.
+  (!help)
+    Show this message.
+  (!chelp)
+    Show command-line option help.
+  (!man)
+    Open the reference manual web page.
+  (!about)
+    Show general and copyright description.
+  (!gc)
+    Run GC manually.
+  !history
+    Get history list (Tuples of (input string, parsed expression, result value).
+  !inputs
+    Get input string history list.
+  !exprs
+    Get parsed expression history list.
+  !results
+    Get result value history list.
+  !continuous
+    Boolean member. Get or set whether the symbols is continuous during REPL.
+    ex) (= !continuous true) ; Default is false.
+  !verbose
+    Boolean member. Get or set whether REPL shows verbose messages.
+    ex) (= !verbosed false) ; Default is true.
+  CODE
+    Run one-line CODE.
+  <<INPUT [ENTER] CODES
+    Run multi-line CODES while INPUT line was got (heredoc <<EOT).
+  CODE
+    Otherwise: Run one-line code.";
+            #endregion
+
+        private const String _chelp =
+            #region String
+@"Command-line Options:
+  YacqRun
+    Run in REPL mode.
+  YacqRun PATH
+    Run script file in PATH.
+  YacqRun -c PATH
+    Compile script file in PATH to Console Application EXE.
+  YacqRun -cw PATH
+    Compile script file in PATH to Windows Application EXE.
+  YacqRun -cl PATH
+    Compile script file in PATH to Library DLL.
+  NOTE: Specify - in PATH means read script data from standard input.";
+            #endregion
+
+        private const String _about =
+            #region String
+@"YACQ <http://www.yacq.net/>
+  Yet Another Compilable Query Language, based on Expression Trees API
+  Language service is provided by Yacq.dll, the assembly name is:
+    {0}
+YACQ Runner (YACQRun) is part of YACQ
+  Runner and Compiler of YACQ
+
+Copyright © 2011 Takeshi KIRIYA (aka takeshik) <takeshik@users.sf.net>
+All rights reserved.
+
+YACQ and YACQ Runner are Free Software; licensed under the MIT License.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the ""Software""), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED ""AS IS"", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.";
+            #endregion
+
+        private static readonly String _assemblyName = typeof(YacqServices).Assembly.GetName().ToString();
+
         internal static Boolean ReplContinuous
         {
             get;
@@ -91,66 +179,23 @@ namespace XSpect.Yacq.Runner
         [YacqSymbol(DispatchTypes.Method, "!help")]
         public static Expression ShowHelp(DispatchExpression e, SymbolTable s, Type t)
         {
-            Console.WriteLine(
-                #region String
-@"Commands:
-  (!exit)
-    Exit this program.
-  (!help)
-    Show this message.
-  (!chelp)
-    Show command-line option help.
-  (!man)
-    Open the reference manual web page.
-  (!about)
-    Show general and copyright description.
-  (!gc)
-    Run GC manually.
-  !history
-    Get history list (Tuples of (input string, parsed expression, result value).
-  !inputs
-    Get input string history list.
-  !exprs
-    Get parsed expression history list.
-  !results
-    Get result value history list.
-  !continuous
-    Boolean member. Get or set whether the symbols is continuous during REPL.
-    ex) (= !continuous true) ; Default is false.
-  !verbose
-    Boolean member. Get or set whether REPL shows verbose messages.
-    ex) (= !verbosed false) ; Default is true.
-  CODE
-    Run one-line CODE.
-  <<INPUT [ENTER] CODES
-    Run multi-line CODES while INPUT line was got (heredoc <<EOT).
-  CODE
-    Otherwise: Run one-line code."
-                #endregion
-);
-            return Expression.Empty();
+            return YacqExpression.Dispatch(
+                DispatchTypes.Method,
+                YacqExpression.TypeCandidate(typeof(Console)),
+                "WriteLine",
+                Expression.Field(null, typeof(ReplSymbols).GetField("_help", BindingFlags.NonPublic | BindingFlags.Static))
+            );
         }
 
         [YacqSymbol(DispatchTypes.Method, "!chelp")]
         public static Expression ShowCommandHelp(DispatchExpression e, SymbolTable s, Type t)
         {
-            Console.WriteLine(
-                #region String
-@"Command-line Options:
-  YacqRun
-    Run in REPL mode.
-  YacqRun PATH
-    Run script file in PATH.
-  YacqRun -c PATH
-    Compile script file in PATH to Console Application EXE.
-  YacqRun -cw PATH
-    Compile script file in PATH to Windows Application EXE.
-  YacqRun -cl PATH
-    Compile script file in PATH to Library DLL.
-  NOTE: Specify - in PATH means read script data from standard input."
-                #endregion
-);
-            return Expression.Empty();
+            return YacqExpression.Dispatch(
+                DispatchTypes.Method,
+                YacqExpression.TypeCandidate(typeof(Console)),
+                "WriteLine",
+                Expression.Field(null, typeof(ReplSymbols).GetField("_chelp", BindingFlags.NonPublic | BindingFlags.Static))
+            );
         }
 
         [YacqSymbol(DispatchTypes.Method, "!man")]
@@ -167,41 +212,13 @@ namespace XSpect.Yacq.Runner
         [YacqSymbol(DispatchTypes.Method, "!about")]
         public static Expression ShowCopying(DispatchExpression e, SymbolTable s, Type t)
         {
-            Console.WriteLine(
-                #region String
-@"YACQ <http://www.yacq.net/>
-  Yet Another Compilable Query Language, based on Expression Trees API
-  Language service is provided by Yacq.dll, the assembly name is:
-    {0}
-YACQ Runner (YACQRun) is part of YACQ
-  Runner and Compiler of YACQ
-
-Copyright © 2011 Takeshi KIRIYA (aka takeshik) <takeshik@users.sf.net>
-All rights reserved.
-
-YACQ and YACQ Runner are Free Software; licensed under the MIT License.
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the ""Software""), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED ""AS IS"", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE."
-                #endregion
-                , typeof(YacqServices).Assembly.GetName()
+            return YacqExpression.Dispatch(
+                DispatchTypes.Method,
+                YacqExpression.TypeCandidate(typeof(Console)),
+                "WriteLine",
+                Expression.Field(null, typeof(ReplSymbols).GetField("_about", BindingFlags.NonPublic | BindingFlags.Static)),
+                Expression.Field(null, typeof(ReplSymbols).GetField("_assemblyName", BindingFlags.NonPublic | BindingFlags.Static))
             );
-            return Expression.Empty();
         }
 
         [YacqSymbol(DispatchTypes.Method, "!gc")]
