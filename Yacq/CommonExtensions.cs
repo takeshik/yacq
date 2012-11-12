ab55e7387123db660129e68cdc88393034530e47
Takeshi KIRIYA <takeshik@xspect.org>
2012-11-12 10:48:55 +0900

    Add extension methods, fix typo
    Add extension methods in CommonExtensions.
    Fix code format and typo in CommonExtensions.

--- Yacq/CommonExtensions.cs
+++ Yacq/CommonExtensions.cs
@@ -40,42 +40,66 @@ namespace XSpect.Yacq
     {
         #region Let
 
-        internal static TReturn Let<TReceiver, TReturn>(this TReceiver self, Func<TReceiver, TReturn> func)
+        internal static TReturn Let<TReceiver, TReturn>(
+            this TReceiver self,
+            Func<TReceiver, TReturn> func
+        )
         {
             return func(self);
         }
 
-        internal static TReturn Let<T1, T2, TReturn>(this Tuple<T1, T2> tuple, Func<T1, T2, TReturn> func)
+        internal static TReturn Let<T1, T2, TReturn>(
+            this Tuple<T1, T2> tuple,
+            Func<T1, T2, TReturn> func
+        )
         {
             return func(tuple.Item1, tuple.Item2);
         }
 
-        internal static TReturn Let<T1, T2, T3, TReturn>(this Tuple<T1, T2, T3> tuple, Func<T1, T2, T3, TReturn> func)
+        internal static TReturn Let<T1, T2, T3, TReturn>(
+            this Tuple<T1, T2, T3> tuple,
+            Func<T1, T2, T3, TReturn> func
+        )
         {
             return func(tuple.Item1, tuple.Item2, tuple.Item3);
         }
 
-        internal static TReturn Let<T1, T2, T3, T4, TReturn>(this Tuple<T1, T2, T3, T4> tuple, Func<T1, T2, T3, T4, TReturn> func)
+        internal static TReturn Let<T1, T2, T3, T4, TReturn>(
+            this Tuple<T1, T2, T3, T4> tuple,
+            Func<T1, T2, T3, T4, TReturn> func
+        )
         {
             return func(tuple.Item1, tuple.Item2, tuple.Item3, tuple.Item4);
         }
 
-        internal static TReturn Let<T1, T2, T3, T4, T5, TReturn>(this Tuple<T1, T2, T3, T4, T5> tuple, Func<T1, T2, T3, T4, T5, TReturn> func)
+        internal static TReturn Let<T1, T2, T3, T4, T5, TReturn>(
+            this Tuple<T1, T2, T3, T4, T5> tuple,
+            Func<T1, T2, T3, T4, T5, TReturn> func
+        )
         {
             return func(tuple.Item1, tuple.Item2, tuple.Item3, tuple.Item4, tuple.Item5);
         }
 
-        internal static TReturn Let<T1, T2, T3, T4, T5, T6, TReturn>(this Tuple<T1, T2, T3, T4, T5, T6> tuple, Func<T1, T2, T3, T4, T5, T6, TReturn> func)
+        internal static TReturn Let<T1, T2, T3, T4, T5, T6, TReturn>(
+            this Tuple<T1, T2, T3, T4, T5, T6> tuple,
+            Func<T1, T2, T3, T4, T5, T6, TReturn> func
+        )
         {
             return func(tuple.Item1, tuple.Item2, tuple.Item3, tuple.Item4, tuple.Item5, tuple.Item6);
         }
 
-        internal static TReturn Let<T1, T2, T3, T4, T5, T6, T7, TReturn>(this Tuple<T1, T2, T3, T4, T5, T6, T7> tuple, Func<T1, T2, T3, T4, T5, T6, T7, TReturn> func)
+        internal static TReturn Let<T1, T2, T3, T4, T5, T6, T7, TReturn>(
+            this Tuple<T1, T2, T3, T4, T5, T6, T7> tuple,
+            Func<T1, T2, T3, T4, T5, T6, T7, TReturn> func
+        )
         {
             return func(tuple.Item1, tuple.Item2, tuple.Item3, tuple.Item4, tuple.Item5, tuple.Item6, tuple.Item7);
         }
 
-        internal static TReturn Let<T1, T2, T3, T4, T5, T6, T7, TRest, TReturn>(this Tuple<T1, T2, T3, T4, T5, T6, T7, TRest> tuple, Func<T1, T2, T3, T4, T5, T6, T7, TRest, TReturn> func)
+        internal static TReturn Let<T1, T2, T3, T4, T5, T6, T7, TRest, TReturn>(
+            this Tuple<T1, T2, T3, T4, T5, T6, T7, TRest> tuple,
+            Func<T1, T2, T3, T4, T5, T6, T7, TRest, TReturn> func
+        )
         {
             return func(tuple.Item1, tuple.Item2, tuple.Item3, tuple.Item4, tuple.Item5, tuple.Item6, tuple.Item7, tuple.Rest);
         }
@@ -84,49 +108,71 @@ namespace XSpect.Yacq
 
         #region Apply
 
-        internal static TReceiver Apply<TReceiver>(this TReceiver self, params Action<TReceiver>[] actions)
+        internal static TReceiver Apply<TReceiver>(
+            this TReceiver self,
+            params Action<TReceiver>[] actions
+        )
         {
             Array.ForEach(actions, a => a(self));
             return self;
         }
 
-        internal static Tuple<T1, T2> Apply<T1, T2>(this Tuple<T1, T2> tuple, params Action<T1, T2>[] actions)
+        internal static Tuple<T1, T2> Apply<T1, T2>(
+            this Tuple<T1, T2> tuple,
+            params Action<T1, T2>[] actions)
         {
             Array.ForEach(actions, a => a(tuple.Item1, tuple.Item2));
             return tuple;
         }
 
-        internal static Tuple<T1, T2, T3> Apply<T1, T2, T3>(this Tuple<T1, T2, T3> tuple, params Action<T1, T2, T3>[] actions)
+        internal static Tuple<T1, T2, T3> Apply<T1, T2, T3>(
+            this Tuple<T1, T2, T3> tuple,
+            params Action<T1, T2, T3>[] actions
+        )
         {
             Array.ForEach(actions, a => a(tuple.Item1, tuple.Item2, tuple.Item3));
             return tuple;
         }
 
-        internal static Tuple<T1, T2, T3, T4> Apply<T1, T2, T3, T4>(this Tuple<T1, T2, T3, T4> tuple, params Action<T1, T2, T3, T4>[] actions)
+        internal static Tuple<T1, T2, T3, T4> Apply<T1, T2, T3, T4>(
+            this Tuple<T1, T2, T3, T4> tuple,
+            params Action<T1, T2, T3, T4>[] actions
+        )
         {
             Array.ForEach(actions, a => a(tuple.Item1, tuple.Item2, tuple.Item3, tuple.Item4));
             return tuple;
         }
 
-        internal static Tuple<T1, T2, T3, T4, T5> Apply<T1, T2, T3, T4, T5>(this Tuple<T1, T2, T3, T4, T5> tuple, params Action<T1, T2, T3, T4, T5>[] actions)
+        internal static Tuple<T1, T2, T3, T4, T5> Apply<T1, T2, T3, T4, T5>(
+            this Tuple<T1, T2, T3, T4, T5> tuple,
+            params Action<T1, T2, T3, T4, T5>[] actions
+        )
         {
             Array.ForEach(actions, a => a(tuple.Item1, tuple.Item2, tuple.Item3, tuple.Item4, tuple.Item5));
             return tuple;
         }
 
-        internal static Tuple<T1, T2, T3, T4, T5, T6> Apply<T1, T2, T3, T4, T5, T6>(this Tuple<T1, T2, T3, T4, T5, T6> tuple, params Action<T1, T2, T3, T4, T5, T6>[] actions)
+        internal static Tuple<T1, T2, T3, T4, T5, T6> Apply<T1, T2, T3, T4, T5, T6>(
+            this Tuple<T1, T2, T3, T4, T5, T6> tuple,
+            params Action<T1, T2, T3, T4, T5, T6>[] actions
+        )
         {
             Array.ForEach(actions, a => a(tuple.Item1, tuple.Item2, tuple.Item3, tuple.Item4, tuple.Item5, tuple.Item6));
             return tuple;
         }
 
-        internal static Tuple<T1, T2, T3, T4, T5, T6, T7> Apply<T1, T2, T3, T4, T5, T6, T7>(this Tuple<T1, T2, T3, T4, T5, T6, T7> tuple, params Action<T1, T2, T3, T4, T5, T6, T7>[] actions)
+        internal static Tuple<T1, T2, T3, T4, T5, T6, T7> Apply<T1, T2, T3, T4, T5, T6, T7>(
+            this Tuple<T1, T2, T3, T4, T5, T6, T7> tuple,
+            params Action<T1, T2, T3, T4, T5, T6, T7>[] actions)
         {
             Array.ForEach(actions, a => a(tuple.Item1, tuple.Item2, tuple.Item3, tuple.Item4, tuple.Item5, tuple.Item6, tuple.Item7));
             return tuple;
         }
 
-        internal static Tuple<T1, T2, T3, T4, T5, T6, T7, TRest> Apply<T1, T2, T3, T4, T5, T6, T7, TRest>(this Tuple<T1, T2, T3, T4, T5, T6, T7, TRest> tuple, params Action<T1, T2, T3, T4, T5, T6, T7, TRest>[] actions)
+        internal static Tuple<T1, T2, T3, T4, T5, T6, T7, TRest> Apply<T1, T2, T3, T4, T5, T6, T7, TRest>(
+            this Tuple<T1, T2, T3, T4, T5, T6, T7, TRest> tuple,
+            params Action<T1, T2, T3, T4, T5, T6, T7, TRest>[] actions
+        )
         {
             Array.ForEach(actions, a => a(tuple.Item1, tuple.Item2, tuple.Item3, tuple.Item4, tuple.Item5, tuple.Item6, tuple.Item7, tuple.Rest));
             return tuple;
@@ -420,6 +466,129 @@ namespace XSpect.Yacq
             return str.EndsWith(value, StringComparison.InvariantCulture);
         }
 
+        internal static IEnumerable<TResult> Zip<TSource1, TSource2, TSource3, TResult>(
+            this IEnumerable<TSource1> source1,
+            IEnumerable<TSource2> source2,
+            IEnumerable<TSource3> source3,
+            Func<TSource1, TSource2, TSource3, TResult> resultSelector
+        )
+        {
+            using (var iter1 = source1.GetEnumerator())
+            using (var iter2 = source2.GetEnumerator())
+            using (var iter3 = source3.GetEnumerator())
+            {
+                while (iter1.MoveNext() && iter2.MoveNext() && iter3.MoveNext())
+                {
+                    yield return resultSelector(iter1.Current, iter2.Current, iter3.Current);
+                }
+            }
+        }
+
+        internal static IEnumerable<TResult> Zip<TSource1, TSource2, TSource3, TSource4, TResult>(
+            this IEnumerable<TSource1> source1,
+            IEnumerable<TSource2> source2,
+            IEnumerable<TSource3> source3,
+            IEnumerable<TSource4> source4,
+            Func<TSource1, TSource2, TSource3, TSource4, TResult> resultSelector
+        )
+        {
+            using (var iter1 = source1.GetEnumerator())
+            using (var iter2 = source2.GetEnumerator())
+            using (var iter3 = source3.GetEnumerator())
+            {
+                while (iter1.MoveNext() && iter2.MoveNext() && iter3.MoveNext())
+                {
+                    yield return resultSelector(iter1.Current, iter2.Current, iter3.Current);
+                }
+            }
+        }
+
+        internal static IEnumerable<TResult> Zip<TSource1, TSource2, TSource3, TSource4, TSource5, TResult>(
+            this IEnumerable<TSource1> source1,
+            IEnumerable<TSource2> source2,
+            IEnumerable<TSource3> source3,
+            IEnumerable<TSource4> source4,
+            IEnumerable<TSource5> source5,
+            Func<TSource1, TSource2, TSource3, TSource4, TSource5, TResult> resultSelector
+        )
+        {
+            using (var iter1 = source1.GetEnumerator())
+            using (var iter2 = source2.GetEnumerator())
+            using (var iter3 = source3.GetEnumerator())
+            {
+                while (iter1.MoveNext() && iter2.MoveNext() && iter3.MoveNext())
+                {
+                    yield return resultSelector(iter1.Current, iter2.Current, iter3.Current);
+                }
+            }
+        }
+
+        internal static IEnumerable<TResult> Zip<TSource1, TSource2, TSource3, TSource4, TSource5, TSource6, TResult>(
+            this IEnumerable<TSource1> source1,
+            IEnumerable<TSource2> source2,
+            IEnumerable<TSource3> source3,
+            IEnumerable<TSource4> source4,
+            IEnumerable<TSource5> source5,
+            IEnumerable<TSource6> source6,
+            Func<TSource1, TSource2, TSource3, TSource4, TSource5, TSource6, TResult> resultSelector
+        )
+        {
+            using (var iter1 = source1.GetEnumerator())
+            using (var iter2 = source2.GetEnumerator())
+            using (var iter3 = source3.GetEnumerator())
+            {
+                while (iter1.MoveNext() && iter2.MoveNext() && iter3.MoveNext())
+                {
+                    yield return resultSelector(iter1.Current, iter2.Current, iter3.Current);
+                }
+            }
+        }
+
+        internal static IEnumerable<TResult> Zip<TSource1, TSource2, TSource3, TSource4, TSource5, TSource6, TSource7, TResult>(
+            this IEnumerable<TSource1> source1,
+            IEnumerable<TSource2> source2,
+            IEnumerable<TSource3> source3,
+            IEnumerable<TSource4> source4,
+            IEnumerable<TSource5> source5,
+            IEnumerable<TSource6> source6,
+            IEnumerable<TSource7> source7,
+            Func<TSource1, TSource2, TSource3, TSource4, TSource5, TSource6, TSource7, TResult> resultSelector
+        )
+        {
+            using (var iter1 = source1.GetEnumerator())
+            using (var iter2 = source2.GetEnumerator())
+            using (var iter3 = source3.GetEnumerator())
+            {
+                while (iter1.MoveNext() && iter2.MoveNext() && iter3.MoveNext())
+                {
+                    yield return resultSelector(iter1.Current, iter2.Current, iter3.Current);
+                }
+            }
+        }
+
+        internal static IEnumerable<TResult> Zip<TSource1, TSource2, TSource3, TSource4, TSource5, TSource6, TSource7, TSource8, TResult>(
+            this IEnumerable<TSource1> source1,
+            IEnumerable<TSource2> source2,
+            IEnumerable<TSource3> source3,
+            IEnumerable<TSource4> source4,
+            IEnumerable<TSource5> source5,
+            IEnumerable<TSource6> source6,
+            IEnumerable<TSource7> source7,
+            IEnumerable<TSource8> source8,
+            Func<TSource1, TSource2, TSource3, TSource4, TSource5, TSource6, TSource7, TSource8, TResult> resultSelector
+        )
+        {
+            using (var iter1 = source1.GetEnumerator())
+            using (var iter2 = source2.GetEnumerator())
+            using (var iter3 = source3.GetEnumerator())
+            {
+                while (iter1.MoveNext() && iter2.MoveNext() && iter3.MoveNext())
+                {
+                    yield return resultSelector(iter1.Current, iter2.Current, iter3.Current);
+                }
+            }
+        }
+
         internal static IEnumerable<TResult> Choose<TSource, TResult>(this IEnumerable<TSource> source, Func<TSource, TResult> selector)
             where TSource : class
         {
@@ -477,12 +646,12 @@ namespace XSpect.Yacq
                 : defaultValue;
         }
 
-        internal static IDictionary<TKey, TValue> ToDitionary<TKey, TValue>(this IEnumerable<KeyValuePair<TKey, TValue>> source)
+        internal static Dictionary<TKey, TValue> ToDictionary<TKey, TValue>(this IEnumerable<KeyValuePair<TKey, TValue>> source)
         {
             return source.ToDictionary(p => p.Key, p => p.Value);
         }
 
-        internal static IDictionary<TKey, TValue> ToDitionary<TKey, TValue>(this IEnumerable<Tuple<TKey, TValue>> source)
+        internal static Dictionary<TKey, TValue> ToDictionary<TKey, TValue>(this IEnumerable<Tuple<TKey, TValue>> source)
         {
             return source.ToDictionary(p => p.Item1, p => p.Item2);
         }
